9a7a6fcd92563f56ac3a8fbbebec6cb5419b99e1
Matthew Ibrahim <105619742+matthewibrahim2025@users.noreply.github.com>
2023-07-10 16:15:57 -0700

    made tables

--- capstone-api/capstone-schema.sql
+++ capstone-api/capstone-schema.sql
@@ -1,9 +1,29 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    acc_value DECIMAL NOT NULL,
+    buying_power DECIMAL NOT NULL,
     email TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 1),
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW())
+    ;
+
+CREATE TABLE portfolio (
+    id SERIAL PRIMARY KEY,
+    ticker TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW())
+    ;
+
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    ticker TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    curr_price DECIMAL NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW())
+    trans_type TEXT NOT NULL,
     ;
