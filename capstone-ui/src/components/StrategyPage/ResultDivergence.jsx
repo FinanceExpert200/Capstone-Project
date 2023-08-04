c7189f476df20172f0855b6adbf810fba57a26e2
Matthew Ibrahim <105619742+matthewibrahim2025@users.noreply.github.com>
2023-08-04 11:53:56 -0700

    polishing

--- capstone-ui/src/components/StrategyPage/ResultDivergence.jsx
+++ capstone-ui/src/components/StrategyPage/ResultDivergence.jsx
@@ -1,93 +1,185 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 import {
-  Heading, Button, Box, Center, Circle, Square, Stack,
-  Text, Flex, Container, Divider, Tag,
-  Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer,
-  Tabs, TabList, TabPanels, Tab, TabPanel
-} from '@chakra-ui/react';
-import SingleStockGraph from '../Graph/SingleStockGraph';
-import { format, parseISO } from "date-fns"
+  Heading,
+  Button,
+  Box,
+  Center,
+  Circle,
+  Square,
+  Stack,
+  Text,
+  Flex,
+  Container,
+  Divider,
+  Tag,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import SingleStockGraph from "../Graph/SingleStockGraph";
+import { format, parseISO } from "date-fns";
 
-export default function ResultDisplay({ accountValues, transactionHistory, rsi, companies, buyingPower }) {
-  const [history, setHistory] = useState([])
-  console.log("The history: ", rsi)
+export default function ResultDisplay({
+  accountValues,
+  transactionHistory,
+  rsi,
+  companies,
+  buyingPower,
+}) {
+  const [history, setHistory] = useState([]);
+  console.log("The history: ", rsi);
   useEffect(() => {
     const updatedHistory = companies.map((company) => {
       return transactionHistory.filter((comp) => comp.ticker === company);
     });
     setHistory(updatedHistory);
-
-  }, [companies, transactionHistory])
+  }, [companies, transactionHistory]);
   //const meta = transactionHistory.filter(company => company.ticker === companies[0])
-  console.log("THE RSI : ", history)
-
+  console.log("THE RSI : ", history);
 
   return (
-    <Flex direction={'column'} w={'full'} h={'full'} mt={10} p={10} textColor={'#03314b'}>
-      <Box bgColor={'#03314b'} minH={'30vh'} w={'full'} p={7}>
-        <Flex direction={'row'} w={'full'} justify={'space-between'} textColor={'white'}>
-          <Heading fontWeight={'light'}>Divergence Strategy</Heading>
-          <Tag fontSize={'23'} bg="whiteAlpha.600">Buying Power : ${buyingPower}</Tag>
-
+    <Flex
+      direction={"column"}
+      w={"full"}
+      h={"full"}
+      mt={10}
+      p={10}
+      textColor={"#03314b"}
+    >
+      <Box bgColor={"#03314b"} minH={"30vh"} w={"full"} p={7}>
+        <Flex direction={"column"} w={"full"} textColor={"white"}>
+          <Flex direction="row" justify="space-between" align="center">
+            <Heading fontWeight={"light"}>Divergence Strategy:</Heading>
+            <Tag fontSize={"23"} bg="whiteAlpha.600">
+              Buying Power : ${buyingPower}
+            </Tag>
+          </Flex>
+          <Text fontSize={30} mt={4}>
+            It watches the RSI and daily price changes. When the price goes down
+            but RSI goes up, it tells us to buy the stock, thinking it might go
+            up later. When the price goes up but RSI goes down, it tells us to
+            sell, thinking the price might go down.
+          </Text>
         </Flex>
-
       </Box>
-      <Container color='#edf0f5' p={4} w={'full'} mt={'-120px'}  h={300} boxShadow={'0,2px,5px,rgba(0,0,0,0.2)'}>
-        <Flex direction={'row'} justify={'space-between'} 
-              bgColor={'#edf0f5'} borderRadius={'5'} 
-              pl={3} pr={3} boxShadow={'20px 20px 30px grey'}>
-          <Square w={'auto'} h={'200px'} 
-            display={'flex'} flexDirection={'column'}
-            justify={'center'}
+
+      <Container
+        color="#edf0f5"
+        p={4}
+        w={"full"}
+        mt={"-120px"}
+        h={300}
+        boxShadow={"0,2px,5px,rgba(0,0,0,0.2)"}
+      >
+        <Flex
+          direction={"row"}
+          justify={"space-between"}
+          bgColor={"#edf0f5"}
+          borderRadius={"5"}
+          pl={3}
+          pr={3}
+          boxShadow={"20px 20px 90px grey"}
+        >
+          <Square
+            w={"auto"}
+            h={"200px"}
+            display={"flex"}
+            flexDirection={"column"}
+            justify={"center"}
           >
-            <Text color={Number(accountValues[0]) < 0 ? 'red.600' : '#1ecc97'} fontSize={30}>${Number(accountValues[0]).toFixed(2)}</Text>
-            <Text color={'gray.500'} fontSize={25}>3 Month Profit</Text>
+            <Text
+              color={Number(accountValues[0]) < 0 ? "red.600" : "#1ecc97"}
+              fontSize={30}
+            >
+              ${Number(accountValues[0]).toFixed(2)}
+            </Text>
+            <Text color={"gray.500"} fontSize={25}>
+              3 Month Profit
+            </Text>
           </Square>
 
           <Box display="flex" alignItems="center">
             <Divider orientation="vertical" h="100px" borderColor="gray.300" />
           </Box>
 
-          <Square w={'auto'} h={'200px'}
-            display={'flex'} flexDirection={'column'}
+          <Square
+            w={"auto"}
+            h={"200px"}
+            display={"flex"}
+            flexDirection={"column"}
           >
-            <Text color={Number(accountValues[1]) < 0 ? 'red.600' : '#1ecc97'} fontSize={30}>${Number(accountValues[1]).toFixed(2)}</Text>
-            <Text color={'gray.500'} fontSize={25}>6 Month Profit</Text>
+            <Text
+              color={Number(accountValues[1]) < 0 ? "red.600" : "#1ecc97"}
+              fontSize={30}
+            >
+              ${Number(accountValues[1]).toFixed(2)}
+            </Text>
+            <Text color={"gray.500"} fontSize={25}>
+              6 Month Profit
+            </Text>
           </Square>
 
           <Box display="flex" alignItems="center">
             <Divider orientation="vertical" h="100px" borderColor="gray.300" />
           </Box>
 
-          <Square w={'auto'} h={'200px'} 
-            display={'flex'} flexDirection={'column'}>
-            <Text color={Number(accountValues[2]) < 0 ? 'red.600' : '#1ecc97'} fontSize={30}>${Number(accountValues[2]).toFixed(2)}</Text>
-            <Text color={'gray.500'} fontSize={25}>1 Year Profit</Text>
+          <Square
+            w={"auto"}
+            h={"200px"}
+            display={"flex"}
+            flexDirection={"column"}
+          >
+            <Text
+              color={Number(accountValues[2]) < 0 ? "red.600" : "#1ecc97"}
+              fontSize={30}
+            >
+              ${Number(accountValues[2]).toFixed(2)}
+            </Text>
+            <Text color={"gray.500"} fontSize={25}>
+              1 Year Profit
+            </Text>
           </Square>
-
         </Flex>
       </Container>
 
-      <Box w={'full'} p={5} bgColor={'#03314b'} borderRadius={10}>
-      <SingleStockGraph data={rsi} dataName="RSI" aspect={4} color={'#b4abaf'} />
+      <Box w={"full"} p={5} bgColor={"#03314b"} borderRadius={10}>
+        <SingleStockGraph
+          data={rsi}
+          dataName="RSI"
+          aspect={4}
+          color={"#b4abaf"}
+        />
       </Box>
 
-      <Flex direction={'row'} justify={'space-between'} >
-      <Tabs variant='enclosed' borderColor={'black'} w={'full'} p={5} >
-          <TabList  >
+      <Flex direction={"row"} justify={"space-between"}>
+        <Tabs variant="enclosed" borderColor={"black"} w={"full"} p={5}>
+          <TabList>
             {companies.map((company) => (
-              <Tab _selected={{ color: 'white', bg: '#03314b' }} 
-              _hover={{ bg: "green.500", color: "white" }}>{company}</Tab>
-            ))
-            }
-
+              <Tab
+                _selected={{ color: "white", bg: "#03314b" }}
+                _hover={{ bg: "green.500", color: "white" }}
+              >
+                {company}
+              </Tab>
+            ))}
           </TabList>
           <TabPanels>
             {history.map((company) => (
-              <TabPanel key={company} overflow="scroll" h={'25vh'}>
+              <TabPanel key={company} overflow="scroll" h={"25vh"}>
                 <TableContainer>
-                  <Table variant='simple'>
+                  <Table variant="simple">
                     <TableCaption>Description</TableCaption>
                     <Thead>
                       <Tr>
@@ -97,12 +189,17 @@ export default function ResultDisplay({ accountValues, transactionHistory, rsi, 
                         <Th isNumeric>Price</Th>
                       </Tr>
                     </Thead>
-                    <Tbody overflow="scroll" h={'20vh'}>
+                    <Tbody overflow="scroll" h={"20vh"}>
                       {company.map((c) => (
-                        <Tr >
+                        <Tr>
                           <Td>{c.ticker}</Td>
                           <Td>{format(parseISO(c.date), "MMM, d, yyyy")}</Td>
-                          <Td><Tag bg="whiteAlpha.600">{c.type.slice(0, 1).toUpperCase() + c.type.slice(1, c.type.length)}</Tag></Td>
+                          <Td>
+                            <Tag bg="whiteAlpha.600">
+                              {c.type.slice(0, 1).toUpperCase() +
+                                c.type.slice(1, c.type.length)}
+                            </Tag>
+                          </Td>
                           <Td isNumeric>${c.price.toFixed(2)}</Td>
                         </Tr>
                       ))}
@@ -119,15 +216,9 @@ export default function ResultDisplay({ accountValues, transactionHistory, rsi, 
                 </TableContainer>
               </TabPanel>
             ))}
-
           </TabPanels>
-
-
         </Tabs>
-
-
       </Flex>
-
     </Flex>
-  )
+  );
 }
--- capstone-ui/src/components/StrategyPage/ResultMovingAverage.jsx
+++ capstone-ui/src/components/StrategyPage/ResultMovingAverage.jsx
@@ -1,120 +1,214 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 import {
-  Heading, Button, Box, Center, Circle, Container, Divider, Square, Stack, Text, Flex,
+  Heading,
+  Button,
+  Box,
+  Center,
+  Circle,
+  Container,
+  Divider,
+  Square,
+  Stack,
+  Text,
+  Flex,
   Tag,
-  Tabs, TabList, TabPanels, Tab, TabPanel,
-  Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer
-} from '@chakra-ui/react';
-import { format, parseISO } from "date-fns"
-import StrategyGraph from '../Graph/StrategyGraph';
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import { format, parseISO } from "date-fns";
+import StrategyGraph from "../Graph/StrategyGraph";
 //maArray, transactionHistory, accountValues
 
-export default function MovingAverageResult({ maArray, transactionHistory, accountValues, companies, buyingPower }) {
-  console.log('Moving Average Result: ', transactionHistory)
+export default function MovingAverageResult({
+  maArray,
+  transactionHistory,
+  accountValues,
+  companies,
+  buyingPower,
+}) {
+  console.log("Moving Average Result: ", transactionHistory);
   const [history, setHistory] = useState([]);
-  const [transHistory, setTransHistory] = useState([])
+  const [transHistory, setTransHistory] = useState([]);
   useEffect(() => {
     const updatedHistory = companies.map((company) => {
       return maArray.filter((comp) => comp.ticker === company);
     });
     setHistory(updatedHistory);
     const updatedTransaction = companies.map((company) => {
-      console.log("company: ", company)
-      return transactionHistory.filter((trans) => trans.ticker === company)
-    })
-    console.log("HISTORY_____ ", updatedHistory)
-    setTransHistory(updatedTransaction)
+      console.log("company: ", company);
+      return transactionHistory.filter((trans) => trans.ticker === company);
+    });
+    console.log("HISTORY_____ ", updatedHistory);
+    setTransHistory(updatedTransaction);
+  }, [companies, transactionHistory]);
 
-  }, [companies, transactionHistory])
-
-  console.log("BO", buyingPower)
+  console.log("BO", buyingPower);
 
   return (
-    <Flex direction={'column'} w={'full'} h={'80vh'} mt={10} p={10} textColor={'#03314b'}>
-      <Box bgColor={'#03314b'} minH={'30vh'} w={'full'} p={7}>
-        <Flex direction={'row'} w={'full'} justify={'space-between'} textColor={'white'}>
-          <Heading fontWeight={'light'}>Moving Average Strategy</Heading>
-          <Tag fontSize={'23'} bg="whiteAlpha.600">Buying Power : ${buyingPower}</Tag>
-          
-
+    <Flex
+      direction={"column"}
+      w={"full"}
+      h={"80vh"}
+      mt={10}
+      p={10}
+      textColor={"#03314b"}
+    >
+      <Box bgColor={"#03314b"} minH={"30vh"} w={"full"} p={7}>
+        <Flex direction={"column"} w={"full"} textColor={"white"}>
+          <Flex direction="row" justify="space-between" align="center">
+            <Heading fontWeight={"light"}>Moving Average Strategy:</Heading>
+            <Tag fontSize={"23"} bg="whiteAlpha.600">
+              Buying Power: ${buyingPower}
+            </Tag>
+          </Flex>
+          <Text fontSize={25} mt={4}>
+            Involves using two different moving averages, one for a shorter
+            period and the other for a longer period. When the short-term
+            average crosses above the long-term average, it's a signal to buy,
+            as recent prices are higher, suggesting the stock may be on the
+            rise. Conversely, when the short-term average crosses below the
+            long-term average, it's a signal to sell, as recent prices are
+            lower, indicating the stock may be on a downward trend.
+          </Text>
         </Flex>
-
       </Box>
-      <Container color='#edf0f5' p={4} w={'full'} mt={'-120px'}  h={300} boxShadow={'0,2px,5px,rgba(0,0,0,0.2)'}>
-        <Flex direction={'row'} justify={'space-between'} 
-              bgColor={'#edf0f5'} borderRadius={'5'} 
-              pl={3} pr={3} boxShadow={'20px 20px 30px grey'}>
-          <Square w={'auto'} h={'200px'} 
-            display={'flex'} flexDirection={'column'}
-            justify={'center'}
+
+      <Container
+        color="#edf0f5"
+        p={4}
+        w={"full"}
+        mt={"-120px"}
+        h={300}
+        boxShadow={"0,2px,5px,rgba(0,0,0,0.2)"}
+      >
+        <Flex
+          direction={"row"}
+          justify={"space-between"}
+          bgColor={"#edf0f5"}
+          borderRadius={"5"}
+          pl={3}
+          pr={3}
+          boxShadow={"20px 20px 90px grey"}
+        >
+          <Square
+            w={"auto"}
+            h={"200px"}
+            display={"flex"}
+            flexDirection={"column"}
+            justify={"center"}
           >
-            <Text color={Number(accountValues[0]) < 0 ? 'red.600' : '#1ecc97'} fontSize={30}>${Number(accountValues[0]).toFixed(2)}</Text>
-            <Text color={'gray.500'} fontSize={25}>3 Month Profit</Text>
+            <Text
+              color={Number(accountValues[0]) < 0 ? "red.600" : "#1ecc97"}
+              fontSize={30}
+            >
+              ${Number(accountValues[0]).toFixed(2)}
+            </Text>
+            <Text color={"gray.500"} fontSize={25}>
+              3 Month Profit
+            </Text>
           </Square>
 
           <Box display="flex" alignItems="center">
             <Divider orientation="vertical" h="100px" borderColor="gray.300" />
           </Box>
 
-          <Square w={'auto'} h={'200px'}
-            display={'flex'} flexDirection={'column'}
+          <Square
+            w={"auto"}
+            h={"200px"}
+            display={"flex"}
+            flexDirection={"column"}
           >
-            <Text color={Number(accountValues[1]) < 0 ? 'red.600' : '#1ecc97'} fontSize={30}>${Number(accountValues[1]).toFixed(2)}</Text>
-            <Text color={'gray.500'} fontSize={25}>6 Month Profit</Text>
+            <Text
+              color={Number(accountValues[1]) < 0 ? "red.600" : "#1ecc97"}
+              fontSize={30}
+            >
+              ${Number(accountValues[1]).toFixed(2)}
+            </Text>
+            <Text color={"gray.500"} fontSize={25}>
+              6 Month Profit
+            </Text>
           </Square>
 
           <Box display="flex" alignItems="center">
             <Divider orientation="vertical" h="100px" borderColor="gray.300" />
           </Box>
 
-          <Square w={'auto'} h={'200px'} 
-            display={'flex'} flexDirection={'column'}>
-            <Text color={Number(accountValues[2]) < 0 ? 'red.600' : '#1ecc97'} fontSize={30}>${Number(accountValues[2]).toFixed(2)}</Text>
-            <Text color={'gray.500'} fontSize={25}>1 Year Profit</Text>
+          <Square
+            w={"auto"}
+            h={"200px"}
+            display={"flex"}
+            flexDirection={"column"}
+          >
+            <Text
+              color={Number(accountValues[2]) < 0 ? "red.600" : "#1ecc97"}
+              fontSize={30}
+            >
+              ${Number(accountValues[2]).toFixed(2)}
+            </Text>
+            <Text color={"gray.500"} fontSize={25}>
+              1 Year Profit
+            </Text>
           </Square>
-
         </Flex>
-
-
       </Container>
 
-
-      <Tabs variant='enclosed' borderColor={'black'} w={'full'} p={5} >
-        <TabList >
+      <Tabs variant="enclosed" borderColor={"black"} w={"full"} p={5}>
+        <TabList>
           {companies.map((company) => (
-            <Tab _selected={{ color: 'white', bg: '#03314b' }} borderTopRadius={5} borderBottomRadius={0}>{company}</Tab>
-          ))
-          }
-
+            <Tab
+              _selected={{ color: "white", bg: "#03314b" }}
+              borderTopRadius={5}
+              borderBottomRadius={0}
+            >
+              {company}
+            </Tab>
+          ))}
         </TabList>
 
-        <TabPanels bgColor={'#03314b'}>
+        <TabPanels bgColor={"#03314b"}>
           {history.map((company) => (
             <TabPanel key={company}>
-              <StrategyGraph data={company} dataName={'percentage'} aspect={4} color={'#b4abaf'} />
+              <StrategyGraph
+                data={company}
+                dataName={"percentage"}
+                aspect={4}
+                color={"#b4abaf"}
+              />
             </TabPanel>
           ))}
-          
         </TabPanels>
-
-
       </Tabs>
-      <Flex direction={'row'} justify={'space-between'}>
-
-        <Tabs variant='enclosed' borderColor={'black'} w={'full'} p={5} >
-          <TabList >
+      <Flex direction={"row"} justify={"space-between"}>
+        <Tabs variant="enclosed" borderColor={"black"} w={"full"} p={5}>
+          <TabList>
             {companies.map((company) => (
-              <Tab _selected={{ color: 'white', bg: '#03314b' }} borderTopRadius={5} borderBottomRadius={0}>{company}</Tab>
-            ))
-            }
-
+              <Tab
+                _selected={{ color: "white", bg: "#03314b" }}
+                borderTopRadius={5}
+                borderBottomRadius={0}
+              >
+                {company}
+              </Tab>
+            ))}
           </TabList>
           <TabPanels>
             {transHistory.map((company) => (
-              <TabPanel key={company} overflow="scroll" h={'25vh'}>
+              <TabPanel key={company} overflow="scroll" h={"25vh"}>
                 <TableContainer>
-                  <Table variant='simple'>
+                  <Table variant="simple">
                     <TableCaption>Description</TableCaption>
                     <Thead>
                       <Tr>
@@ -124,12 +218,17 @@ export default function MovingAverageResult({ maArray, transactionHistory, accou
                         <Th isNumeric>Price</Th>
                       </Tr>
                     </Thead>
-                    <Tbody overflow="scroll" h={'20vh'}>
+                    <Tbody overflow="scroll" h={"20vh"}>
                       {company.map((c) => (
-                        <Tr >
+                        <Tr>
                           <Td>{c.ticker}</Td>
                           <Td>{format(parseISO(c.date), "MMM, d, yyyy")}</Td>
-                          <Td><Tag bg="whiteAlpha.600">{c.type.slice(0, 1).toUpperCase() + c.type.slice(1, c.type.length)}</Tag></Td>
+                          <Td>
+                            <Tag bg="whiteAlpha.600">
+                              {c.type.slice(0, 1).toUpperCase() +
+                                c.type.slice(1, c.type.length)}
+                            </Tag>
+                          </Td>
                           <Td isNumeric>${c.price.toFixed(2)}</Td>
                         </Tr>
                       ))}
@@ -146,15 +245,9 @@ export default function MovingAverageResult({ maArray, transactionHistory, accou
                 </TableContainer>
               </TabPanel>
             ))}
-
           </TabPanels>
-
-
         </Tabs>
-
       </Flex>
-
-
     </Flex>
-  )
+  );
 }
