2eb4c321a856c884003dfdca6d253565f85e0a21
Matthew Ibrahim <105619742+matthewibrahim2025@users.noreply.github.com>
2023-08-08 13:44:52 -0700

    aghh

--- capstone-api/models/transactions.js
+++ capstone-api/models/transactions.js
@@ -1,7 +1,7 @@
 const { use } = require("../Routes/auth");
 const db = require("../db");
 const { BadRequestError, UnauthorizedError } = require("../utils/errors");
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
 class Transaction {
   //Function that gets the transaction history for a particular user
