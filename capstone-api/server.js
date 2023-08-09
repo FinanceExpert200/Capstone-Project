44c522fc03196f4f16958403a06a97cbb4217bf4
Matthew Ibrahim <105619742+matthewibrahim2025@users.noreply.github.com>
2023-08-08 20:31:16 -0700

    removing cors to fix later

--- capstone-api/server.js
+++ capstone-api/server.js
@@ -13,32 +13,20 @@ const { BadRequestError, NotFoundError } = require("./utils/errors"); // Import 
 
 const app = express();
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "https://stock-swap.onrender.com",
-    "https://stock-swap.onrender.com",
-    "https://capstone-project-6ssi.onrender.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
+// app.use(function(req, res, next) {
+//   // res.header("Access-Control-Allow-Origin", "*");
+//   const allowedOrigins = ['https://stock-swap.onrender.com', 'https://stock-swap.onrender.com', "https://capstone-project-6ssi.onrender.com"];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//        res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header("Access-Control-Allow-credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+//   next();
+// });
 
-// app.use(
-//   cors({
-//     origin: "https://capstone-project-6ssi.onrender.com",
-//     withCredentials: true,
-//   })
-// );
+app.use(cors());
 
 app.use(express.json());
 
